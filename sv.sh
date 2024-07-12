14d3a04aa7233a558e45dbbc7b312139dcd14e7e
NishNovae <raecrowned@gmail.com>
2024-07-12 11:19:44 +0900

    save logs to separate directory

--- sv.sh
+++ sv.sh
@@ -2,5 +2,5 @@
 
 DATE=$(date +"%Y%m%d%H%M%S")
 
-cat ~/.zsh_history | cut -d';' -f2- | sort | uniq > history_$DATE.log
+cat ~/.zsh_history | cut -d';' -f2- | sort | uniq > history/history_$DATE.log
 echo history log created: history_$DATE!
